f4a4335f6e728524a9a2ed9f245e74088115503c
Iván Pagano <ivanpgn87@gmail.com>
2021-04-13 12:00:22 -0300

    Ayudas

--- src/app/prueba/prueba.component.ts
+++ src/app/prueba/prueba.component.ts
@@ -7,8 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PruebaComponent implements OnInit {
 
+  /*
+    Acá podemos declarar miembros de la clase. Por ejemplo campos / variables.
+
+    Por ejemplo:
+
+    public contador: number;
+
+    (visibilidad / nombre / tipo de dato)
+  */
+
+
+  /*
+    El constructor por el momento no lo usaremos. No introducir lógica acá, dado que el componente de Angular aún no está vivo.
+  */
   constructor() { }
 
+  /*
+    ngOnInit es el inicio del componente de Angular, cualquier lógica (como un setInterval()) va dentro de este método :)
+  */
   ngOnInit(): void {
   }
 
